4d9402535828937fef401d382421d2257df8586c
Michael Brothers <mBrothers@inspiresemi.com>
2025-11-07 14:19:48 -0600

    invoking build should be a full build by default.

--- scripts/invoke_build_thunderbird.bash
+++ scripts/invoke_build_thunderbird.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-./scripts/build_thunderbird.sh --prefix /mnt/localstore/tcl_demo/llvm-project/myinstall --src ./ --jobs 8 --skip-host --skip-libomp
+./scripts/build_thunderbird.sh --prefix /mnt/localstore/tcl_demo/llvm-project/myinstall --src ./ --jobs 32
